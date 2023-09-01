5c1749c08ff561e0f66a637351bad4ad7030b743
NoumanNawaz444 <noumannawazwork@gmail.com>
2023-09-01 08:28:50 +0500

    about page done

--- assets/js/about.js
+++ assets/js/about.js
@@ -35,6 +35,11 @@ let hobies_heading = document.getElementById('hobies_heading')
 let hobby = document.getElementById('hobby')
 let edu_heading = document.getElementById('edu_heading')
 let edu = document.getElementById('edu')
+let skill_heading = document.getElementById('skill_heading')
+let skills = document.getElementById('skills')
+let interest_heading = document.getElementById('interest_heading')
+let Interest = document.getElementById('Interest')
+let foooter = document.getElementById('foooter')
 
 
 
@@ -42,11 +47,15 @@ day_night.addEventListener('click' , day_night_toggle_about)
 
 function day_night_toggle_about(){
     main_nav.classList.toggle('day_night_toggle')
+    foooter.classList.toggle('day_night_toggle')
     body.classList.toggle('bg_black')
     intro.classList.toggle('bg_dark');
     hobies_heading.classList.toggle('bg_dark');
     edu_heading.classList.toggle('bg_dark');
+    skill_heading.classList.toggle('bg_dark');
+    interest_heading.classList.toggle('bg_dark');
     hobby.classList.toggle('bg_dark');
     edu.classList.toggle('bg_dark');
-
+    skills.classList.toggle('bg_dark');
+    Interest.classList.toggle('bg_dark');
 }
